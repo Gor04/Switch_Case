dde65aa0bb36ce13e28fc8c026f078169be75a6d
Gor Grigoryan <gor.grigoryan0609@gmail.com>
2019-10-07 15:12:32 +0400

    first commit

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
 var myElementId = document.getElementById("name");
 var a = document.getElementById("hex");
 function myFunction1(){
+
 	switch(myElementId.value){
-		case "yellow": 
+		case "Yellow": 
+		case "yellow":
 			document.getElementById("hex").innerHTML = "#FFFF00";
 			document.getElementById('col_div').style.backgroundColor = "yellow";
 			break;
 		case "red":
+		case "Red":
 			document.getElementById("hex").innerHTML = "#FF0000";
 			document.getElementById('col_div').style.backgroundColor = "red";
 			break;
 		case "black":
+		case "Black":
 			document.getElementById("hex").innerHTML = "#FFFFFF";
 			document.getElementById('col_div').style.backgroundColor = "black";
 			break;
-		default :
+		default:
 			document.getElementById("hex").innerHTML = "No Color";
+			document.getElementById('col_div').style.backgroundColor = "transparent";
 			break;
 
 	}
 }
-// style="height: 20px;width: 20px;color: yellow;"
