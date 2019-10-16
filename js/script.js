b63af9379872f58b1efe247ec87b66e0bc4f1bbc
Gor Grigoryan <gor.grigoryan0609@gmail.com>
2019-10-16 16:30:45 +0400

    switch case 2

--- js/script.js
+++ js/script.js
@@ -1,27 +1,69 @@
-var myElementId = document.getElementById("name");
-var a = document.getElementById("hex");
-function myFunction1(){
+// var myElementId = document.getElementById("name");
+// var a = document.getElementById("hex");
+// function myFunction1(){
+// 	var str = myElementId.value.toLowerCase();
+// 	switch(str){
+// 		case "yellow": 
+// 			document.getElementById("hex").innerHTML = "#FFFF00";
+// 			document.getElementById('col_div').style.backgroundColor = "yellow";
+// 			break;
+// 		case "red":
+// 			document.getElementById("hex").innerHTML = "#FF0000";
+// 			document.getElementById('col_div').style.backgroundColor = "red";
+// 			break;
+// 		case "black":
+// 			document.getElementById("hex").innerHTML = "#FFFFFF";
+// 			document.getElementById('col_div').style.backgroundColor = "black";
+// 			break;
+// 		default:
+// 			document.getElementById("hex").innerHTML = "No Color";
+// 			document.getElementById('col_div').style.backgroundColor = "transparent";
+// 			break;
 
-	switch(myElementId.value){
-		case "Yellow": 
-		case "yellow":
-			document.getElementById("hex").innerHTML = "#FFFF00";
-			document.getElementById('col_div').style.backgroundColor = "yellow";
-			break;
-		case "red":
-		case "Red":
-			document.getElementById("hex").innerHTML = "#FF0000";
-			document.getElementById('col_div').style.backgroundColor = "red";
-			break;
-		case "black":
-		case "Black":
-			document.getElementById("hex").innerHTML = "#FFFFFF";
-			document.getElementById('col_div').style.backgroundColor = "black";
-			break;
-		default:
-			document.getElementById("hex").innerHTML = "No Color";
-			document.getElementById('col_div').style.backgroundColor = "transparent";
-			break;
+// 	}
+// }
 
-	}
-}
+// function test_prime(n){
+// 	var a = true;
+// 	if(n <=1){
+// 		a = false;
+// 	}
+// 	else{
+// 		for (var i = 2; i <= Math.sqrt(n); i++) {
+// 			if (n%i==0) {
+// 				a = false;
+// 				break;
+// 			}
+// 		}
+// 	}
+// 	console.log(a);
+// }
+// test_prime(16);
+
+//  function func23(){
+//   function d(arg){
+
+// console.log(typeof arg)
+// }
+// d('a')
+// d(11)
+// d(true)
+// function random(length) {
+// 	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz";
+// 	var randomNumber = '';
+// 	for (var i=0; i<length; i++) {
+// 		randomNumber += chars.charAt(Math.floor(Math.random() * chars.length));
+// 		// randomstring += chars.substring(randomNumber,randomNumber+1);
+
+// 	}
+// 	console.log(randomNumber)
+	
+// }
+
+// random(5);
+
+
+
+
+
+
